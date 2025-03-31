38977b6005db588962c20bfa7207facbf36dc9a0
Tedpronounced-Ari <197651793+Tedpronounced-Ari@users.noreply.github.com>
2025-03-31 17:43:12 +0000

    m

--- blackjack.js
+++ blackjack.js
@@ -1,14 +1,15 @@
-let dealerCards = 0; 
-let playerCards = 0;
-
 function getCards() {
     return Math.floor(Math.random ()*12);
 }
 
 function startGame() {
-    dealerCards = getCards()+ getCards();
+    dealerCardsTotal = getCards()+ getCards();
+    dealerCards1 = getCards(); 
+    dealerCards2 = getCards();
+    dealerCardsTotal = dealerCards1 + dealerCards2
+
     playerCards = getCards()+ getCards();
-    console.log("you have a total of" + playerCards)
+    console.log("you have a total of" + dealerCardsTotal)
 }
 
 startGame (); 
